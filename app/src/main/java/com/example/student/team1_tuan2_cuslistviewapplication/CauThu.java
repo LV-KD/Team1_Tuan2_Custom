0e5e2a7039465097afec89610cb446844ee79de5
= <thien010398@gmail.com>
2019-03-03 18:32:33 +0700

    Thiet ke va hien thi ds len GridView

---
+++ app/src/main/java/com/example/student/team1_tuan2_cuslistviewapplication/CauThu.java
@@ -0,0 +1,40 @@
+package com.example.student.team1_tuan2_cuslistviewapplication;
+
+public class CauThu {
+    private String tenCauThu;
+    private int imgCauthu, imgQuocGia;
+
+    public String getTenCauThu() {
+        return tenCauThu;
+    }
+
+    public void setTenCauThu(String tenCauThu) {
+        this.tenCauThu = tenCauThu;
+    }
+
+    public int getImgCauthu() {
+        return imgCauthu;
+    }
+
+    public void setImgCauthu(int imgCauthu) {
+        this.imgCauthu = imgCauthu;
+    }
+
+    public int getImgQuocGia() {
+        return imgQuocGia;
+    }
+
+    public void setImgQuocGia(int imgQuocGia) {
+        this.imgQuocGia = imgQuocGia;
+    }
+
+    public CauThu(String tenCauThu, int imgCauthu, int imgQuocGia) {
+        this.tenCauThu = tenCauThu;
+        this.imgCauthu = imgCauthu;
+        this.imgQuocGia = imgQuocGia;
+    }
+
+    public CauThu() {
+
+    }
+}
--- app/src/main/java/com/example/student/team1_tuan2_cuslistviewapplication/MainActivity.java
+++ app/src/main/java/com/example/student/team1_tuan2_cuslistviewapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.student.team1_tuan2_cuslistviewapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void IntentCustomGridViewActivity(View view) {
+        Intent intent = new Intent(MainActivity.this,CustomGridViewActivity.class);
+        startActivity(intent);
     }
 }
